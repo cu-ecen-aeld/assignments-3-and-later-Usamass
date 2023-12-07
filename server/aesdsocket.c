14f3e5fef788d33923fdecc1f04f04c92f564f71
osama-shafiq <hafizusamashafiq@gmail.com>
2023-12-07 17:47:10 +0500

    resolve bind error

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -4,17 +4,35 @@
 #include <netdb.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <signal.h>
+
+char msg[200] = "connected to the server\n";
+int sock_fd , s , conn_fd;
 
 
+static void signal_handler() 
+{
+    printf("server shutdown!\n");
+    close(conn_fd);
+    exit(EXIT_SUCCESS);
+
+
+
+}
 int main (int argc , char* argv[]) 
 {
-    int sock_fd , s;
+    // int sock_fd , s , conn_fd;
+
+    /*Signal handling*/
+    signal(SIGINT , signal_handler);
     struct addrinfo addr_info , *res;
+    struct sockaddr client_info;
     memset(&addr_info , 0 , sizeof(addr_info));
     openlog("writer" , LOG_CONS | LOG_PID , LOG_USER);
     syslog(LOG_USER , "This is an assignment5");
 
-    sock_fd = socket(SOCK_STREAM , AF_INET , 0);
+    sock_fd = socket(AF_INET , SOCK_STREAM , 0);
 
     if (sock_fd == -1) {
         syslog(LOG_ERR , "error ocurred while creating socket!\n");
@@ -27,6 +45,11 @@ int main (int argc , char* argv[])
     addr_info.ai_flags = AI_PASSIVE;
 
     s = getaddrinfo(NULL , "9999" , &addr_info , &res);
+    if (s != 0) {
+            fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+            exit(EXIT_FAILURE);
+        }
+
 
     if (s != 0) {
         syslog(LOG_ERR , "error in getaddrinfo\n");
@@ -34,15 +57,26 @@ int main (int argc , char* argv[])
         exit(EXIT_FAILURE);
     }
 
-    if (bind(sock_fd , res->ai_addr , sizeof(res->ai_addr) != 0)) {
+    if (bind(sock_fd , res->ai_addr , res->ai_addrlen) != 0) {
         syslog(LOG_ERR , "error in binding\n");
         closelog();
         exit(EXIT_FAILURE);
 
     }
-
     freeaddrinfo(res);
 
+    listen(sock_fd , 5);
+
+    socklen_t client_len = sizeof(struct sockaddr);
+
+    while (1) {
+        conn_fd = accept(sock_fd , &client_info , &client_len);
+        write(conn_fd , msg , strlen(msg));
+        
+        close(conn_fd);
+        sleep(1);
+
+    }
 
 
 
