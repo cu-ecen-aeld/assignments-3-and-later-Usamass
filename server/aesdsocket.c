3f248fdb19db55a76f63762961ab87a5a892b996
Osama Shafiq <hafizusamashafiq@gmail.com>
2023-12-10 22:13:33 +0500

    testing prev updates

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -6,12 +6,18 @@
 #include <string.h>
 #include <unistd.h>
 #include <signal.h>
+#include <arpa/inet.h>
 
 #define DEBUG_LOG(msg,...) syslog(LOG_DEBUG ,"SOCK_DEBUG: " msg "\n" , ##__VA_ARGS__)
+#define USER_LOG(msg,...) syslog(LOG_USER ,"SOCK_USER: " msg "\n" , ##__VA_ARGS__)
 #define ERROR_LOG(msg,...) syslog(LOG_ERR ,"SOCK_ERROR: " msg "\n" , ##__VA_ARGS__)
 
 char msg[200] = "connected to the server\n";
+char client_ip[INET_ADDRSTRLEN] = {0};
+char recv_buffer[1024] = {0};
 int sock_fd , s , conn_fd;
+
+char file_path[] = "/var/tmp/aesdsocketdata";
 
 
 static void signal_handler() 
@@ -35,11 +41,13 @@ static void infinit_write(int peer_fd)
 }
 int main (int argc , char* argv[]) 
 {
+    ssize_t data_size = 0;
     /*Signal handling*/
     signal(SIGINT , signal_handler);
 
     struct addrinfo addr_info , *res;
     struct sockaddr client_info;
+    struct sockaddr_in* ipv4_client_addr = (struct sockaddr_in*)&client_info;
     memset(&addr_info , 0 , sizeof(addr_info));
     openlog("writer" , LOG_CONS | LOG_PID , LOG_USER);
     syslog(LOG_USER , "This is an assignment5");
@@ -78,8 +86,22 @@ int main (int argc , char* argv[])
     while (1) {
         printf("waiting to accept connection!\n");
         conn_fd = accept(sock_fd , &client_info , &client_len);
+        inet_ntop(AF_INET , &(ipv4_client_addr->sin_addr) , client_ip , sizeof(client_ip));
+        USER_LOG("Accepted connection from: %s" , client_ip);
         printf("connection established!\n");
-        infinit_write(conn_fd);
+
+        data_size = recv(conn_fd , recv_buffer , sizeof(recv_buffer) , 0);
+        char *newline_position = strchr(recv_buffer, '\n'); // Find the position of '\n'
+
+        if (newline_position != NULL) {
+            printf("Newline found at position: %ld\n", newline_position - recv_buffer);
+        } else {
+            printf("Newline not found in the message.\n");
+        }
+        printf("size: %ld \n data received: %s" , data_size , recv_buffer);
+
+
+
 
         // write(conn_fd , msg , strlen(msg));
         // close(conn_fd);
